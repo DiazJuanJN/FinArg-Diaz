9b334b693564a19c52a91cf321be075cd0b857b8
diazjuanj <juanjosediaz842@gmail.com>
2022-11-06 23:13:08 -0300

    Add tabla de Portales de Compra

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+//CONVERSOR DE DIVISA
+
 let valor = prompt("Ingrese el monto sobre el que desea operar:");
 let operacion = prompt("Ingrese la operacion que desea realizar:\n1. Convertir a Dolar Oficial\n2. Convertir a Dolar Tarjeta\n3. Convertir a Dolar Tarjeta (si tuviste gastos de >300U$D)\n4. Convertir a Dolar Oficial con valor del 01/01/2022\n5. Convertir a Dolar Blue.")
 
@@ -43,3 +45,35 @@ const answer4 = document.querySelector("#answer4");
 answer4.textContent = dolarAhorro(valor).toFixed(2);
 const answer5 = document.querySelector("#answer5");
 answer5.textContent = dolarBlue(valor).toFixed(2);
+
+//PORTALES DE COMPRA
+
+function renderTableRows(rows) {
+    let htmlTable = `<tr>
+    <td>label here</td>
+    <td>img here</td>
+    <td>value here</td>
+    </tr>`;
+    let htmlTotal = ``;
+    rows.forEach(function(row) {
+        htmlTable = `<tr>
+        <td>${row[0]}</td>
+        <td><img class="table-logo" src=${row[1]}></td>
+        <td>${row[2]}</td>
+        </tr>`
+        htmlTotal = htmlTotal + htmlTable;
+    })
+    console.log(rows);
+    return htmlTotal;
+}
+
+const data = [
+    ["Uala", "assets/img/uala-logo.png", "$200(+65%)"],
+    ["Brubank", "assets/img/brubank-logo.png", "$180(+50%)"], 
+    ["Banco Nacion", "assets/img/bna-logo.png", "$250(+45%)"]
+];
+
+const html = renderTableRows(data);
+
+const tbody = document.querySelector("#nutrition-table tbody");
+tbody.insertAdjacentHTML("beforeend", html);
