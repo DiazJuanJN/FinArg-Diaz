8289fb2d3bd4ccd300cfed04a7c1d2dcd735e0ce
diazjuanj <juanjosediaz842@gmail.com>
2022-11-19 23:13:15 -0300

    Implementacion select Organizar y Filtro

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,40 +45,88 @@ valorARS.addEventListener("keyup", () => {
 
 //PORTALES DE COMPRA
 
-/*const data = [
-    ["Uala", "assets/img/uala-logo.png", "$200 (+65%)"],
-    ["Brubank", "assets/img/brubank-logo.png", "$180 (+50%)"], 
-    ["Banco Nacion", "assets/img/bna-logo.png", "$250 (+45%)"]
-];*/
-
 const portales = [
-    {nombre: "Uala", logo: "uala-logo.png", cotizacion: "$140 (+65%)"},
-    {nombre: "Brubank", logo: "brubank-logo.png", cotizacion: "$180 (+50%)"},
-    {nombre: "Banco Nacion", logo: "bna-logo.png", cotizacion: "$150 (+45%)"},
-    {nombre: "Binance", logo: "binance-logo.png", cotizacion: "$265 (0%)*"},
-    {nombre: "Lemon Cash", logo: "lemon-logo.png", cotizacion: "$284 (0%)*"},
-    {nombre: "Invertir Online", logo: "invonline-logo.png", cotizacion: "$289 (0%)**"}
-]
+    {nombre: "Uala", tipo: "virtual", logo: "uala-logo.png", cotizacion: "140", imp:"65"},
+    {nombre: "Brubank", tipo: "virtual", logo: "brubank-logo.png", cotizacion: "180", imp: "50"},
+    {nombre: "Banco Nacion", tipo: "bank", logo: "bna-logo.png", cotizacion: "150", imp: "45"},
+    {nombre: "Binance", tipo: "crypto", logo: "binance-logo.png", cotizacion: "265", imp: "0"},
+    {nombre: "Lemon Cash", tipo: "crypto", logo: "lemon-logo.png", cotizacion: "284", imp: "0"},
+    {nombre: "Invertir Online", tipo: "investment", logo: "invonline-logo.png", cotizacion: "289", imp:"0"}
+];
 
 function renderTableRows() {
-    let htmlTable = `<tr>
-    <td>label here</td>
-    <td>img here</td>
-    <td>value here</td>
-    </tr>`;
+    let htmlTable = ``;
     let htmlTotal = ``;
     portales.forEach(portal=>{
         htmlTable = `<tr>
         <td>${portal.nombre}</td>
         <td><img class="table-logo" src="assets/img/${portal.logo}"></td>
-        <td>${portal.cotizacion}</td>
+        <td>$${portal.cotizacion}(+${portal.imp}%)</td>
         </tr>`
         htmlTotal = htmlTotal + htmlTable;
     });
     return htmlTotal;
 }
 
-const html = renderTableRows(portales);
+function renderHTMLTable() {
+    const html = renderTableRows(portales);
+    const tbody = document.querySelector("#portales-table tbody");
+    tbody.innerHTML = ``;
+    tbody.insertAdjacentHTML("beforeend", html);
+}
+renderHTMLTable();
 
-const tbody = document.querySelector("#portales-table tbody");
-tbody.insertAdjacentHTML("beforeend", html);
+//SELECT FILTRO Y ORGANIZAR
+
+const sort = document.querySelector("#sort");
+const filter = document.querySelector("#filter");
+
+sort.addEventListener('change', ()=>{
+    const option1 = sort.options[sort.selectedIndex].value;
+    if (option1 == "alf") {
+        portales.sort((a,b) => (a.nombre > b.nombre) ? 1 : ((b.nombre > a.nombre) ? -1 : 0));
+    }else if (option1 == "lower") {
+        portales.sort((a,b) => (a.cotizacion > b.cotizacion) ? 1 : ((b.cotizacion > a.cotizacion) ? -1 : 0));
+    }else if (option1 == "higher") {
+        portales.sort((a,b) => (a.cotizacion < b.cotizacion) ? 1 : ((b.cotizacion < a.cotizacion) ? -1 : 0));
+    } else if (option1 == "lower-imp") {
+        portales.sort((a,b) => (a.imp > b.imp) ? 1 : ((b.imp > a.imp) ? -1 : 0));
+    }
+    renderHTMLTable();
+});
+
+filter.addEventListener('change', ()=>{
+    let portales2 = [];
+
+    function renderTableRows() {
+        let htmlTable = ``;
+        let htmlTotal = ``;
+        portales2.forEach(portal=>{
+            htmlTable = `<tr>
+            <td>${portal.nombre}</td>
+            <td><img class="table-logo" src="assets/img/${portal.logo}"></td>
+            <td>$${portal.cotizacion}(+${portal.imp}%)</td>
+            </tr>`
+            htmlTotal = htmlTotal + htmlTable;
+        });
+        return htmlTotal;
+    }
+
+    const option2 = filter.options[filter.selectedIndex].value;
+    if (option2 == "virtual") {
+        portales2 = portales.filter(portal => portal.tipo == "virtual");
+    }else if (option2 == "bank") {
+        portales2 = portales.filter(portal => portal.tipo == "bank");
+    }else if (option2 == "crypto") {
+        portales2 = portales.filter(portal => portal.tipo == "crypto");
+    }else if (option2 == "investment") {
+        portales2 = portales.filter(portal => portal.tipo == "investment");
+    }else if (option2 == "showall") {
+        portales2 = portales;
+    }
+    
+    const html = renderTableRows(portales2);
+    const tbody = document.querySelector("#portales-table tbody");
+    tbody.innerHTML = ``;
+    tbody.insertAdjacentHTML("beforeend", html);
+});
